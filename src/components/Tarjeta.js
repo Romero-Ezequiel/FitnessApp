5ea7a1ad8172f265e47b79458f1a84d2eee3ddf4
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-05-23 23:16:07 -0300

    Componente de Tarjeta

---
+++ src/components/Tarjeta.js
@@ -0,0 +1,66 @@
+import React from 'react';
+//import imagen1 from '../img/imagen1.png';
+//Importo los estilos 
+import './style/Tarjeta.css';
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+class Tarjeta extends React.Component{
+
+    // constructor(props){
+    //     //Aca las props se la pasamos al constructor base
+    //     super(props)
+    //     this.state = {
+    //         imagen: 'https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/bulbasaur.png?alt=media&token=567caf19-af47-414e-a9d4-3854ab24c7dc'
+    //     }
+    // }
+
+    //componentDidMount() es una funcion que se invoca inmediatamente despues
+    //que un componente es montado, esto forma parte del ciclo de vida 
+    //de un componente
+    // componentDidMount(){
+    //     setTimeout(()=>{
+    //         //La forma correcta de actualizarle el estado de nuestro 
+    //         //componente 
+    //         this.setState({
+    //             //Aca le indico un nuevo valor al estado y lo modifico  
+    //             //luego de pasar 5 segundos
+    //             imagen: 'https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06'
+    //         })
+    //     }, 5000);
+    // }
+
+    render(){
+
+        //const { title, description } = this.props;
+        
+        const { title, description, img, leftColor, rightColor } = this.props
+
+
+        return (
+                <div className="card mx-auto Fitness-Card"
+                    style={{
+                        backgroundImage: ` linear-gradient(to right, ${leftColor}, ${rightColor})`
+                    }}
+                >                
+                    <div className="card-body">
+                        <div className="row center">
+                            <div className="col-6">
+                                <img src={img} alt="Imagen" className="float-right" />
+                            </div>
+
+                            <div className="col-6 Fitness-Card-Info">
+                                <h1>{title}</h1>
+                                <p>{description}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+           
+        );
+    }
+
+}
+
+export default Tarjeta;
